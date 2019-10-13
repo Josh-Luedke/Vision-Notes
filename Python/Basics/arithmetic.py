58fadbcc2c2b0a4225a203c7a834836e3a0b24e9
Joshua Luedke <ArchitectureGuy55@gmail.com>
2019-10-13 12:43:23 -0400

    Create arthmetic.py

---
+++ Python/Basics/arithmetic.py
@@ -0,0 +1,19 @@
+num1 = 10
+num2 = 100
+
+# Addition 
+numSum = num1 + num2
+print(numSum) # Output of this print statement is 110
+
+# Subtraction
+numDifference = num2 - num1
+print(numDifference) # Output of this print statement is 90
+
+# Division 
+numQuotient = num1 / num2
+print(numQuotient) # Output of this print statement is 0.1
+
+# Multiplication 
+numProduct = num1 * num2
+print(numProduct) # Output of this print statement is 1000
+
