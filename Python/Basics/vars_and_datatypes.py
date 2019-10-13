3a40abb198d0b06f8dada6219b714e5fea9bbb02
Joshua Luedke <ArchitectureGuy55@gmail.com>
2019-10-13 12:14:03 -0400

    Update vars_and_datatypes.py

--- Python/Basics/vars_and_datatypes.py
+++ Python/Basics/vars_and_datatypes.py
@@ -1,5 +1,19 @@
+# Basic Variables
 firstName = "Josh"
 lastName = "Luedke"
 print(firstName)
 print(lastName)
 
+# Strings (Str)
+# A string is a set of characters. Strings can be numbers, letters, or certain symbols. 
+school = "GHS" # This is a string! We know that it's a string because its wrapped in double quotes.
+town = 'New Boston'# This is also a string! We can also use single quotes for strings.
+
+# Integers (Int) 
+# A Int is always a whole numbers. Int's can be positive or negative.
+currentYear = 2019 # This is a int! We know that it is a a int because it is a whole number without a decimal. 
+currentMonth = 10.0 # Even though we know that this is a whole number it's an float because there is a decimal point. 
+
+# Floats 
+# A float is a number with a decimal. Floats can be positive or negative. 
+currentDay = 10.13 # This is a float! We know that it is a float because it has a decimal point. 
