89d95f0bbaf6ae1258ece3a3bb9d932068414b63
Joshua Luedke <ArchitectureGuy55@gmail.com>
2019-10-22 19:46:16 -0400

    Create Variables_and_Data_Types Folder

---
+++ Python/Free_Code_Camp/Variables_and_Data_Types/main.py
@@ -0,0 +1,6 @@
+# Variable is a container for data values
+
+print("There once was a man named George")
+print("he was 70 years old.")
+print("He really liked the name George,")
+print("but didn't like being 70.")
