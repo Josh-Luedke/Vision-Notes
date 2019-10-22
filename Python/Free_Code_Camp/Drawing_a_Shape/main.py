0b4b342ac1e7e565fa587017daf7c5bdf75ba07a
Joshua Luedke <ArchitectureGuy55@gmail.com>
2019-10-22 19:46:58 -0400

    Create Free_Camp_Code Folder

---
+++ Python/Free_Code_Camp/Drawing_a_Shape/main.py
@@ -0,0 +1,12 @@
+# Writing Basic Python Programs 
+# Drawing in Python
+print("   /|")
+print("  / |")
+print(" /  |")
+print("/___|")
+
+# Click Play or Run Button in top right to run program
+# After clicking run the program will appear in the consule 
+# Order of instructions matters!
+
+  
